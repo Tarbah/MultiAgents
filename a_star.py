df18ff7125a69e500e67b58952d729ed7c001840
ElnazShy <elnaz.shafipour@gmail.com>
2018-03-10 22:48:32 +0000

    Some Changes in astar to make it more efficient.

--- a_star.py
+++ a_star.py
@@ -37,20 +37,17 @@ class node:
 
     # A-star algorithm.
     # Path returned will be a string of digits of directions.
+
 class a_star:
 
-    def __init__(self, the_map , dx, dy):
-        # print
-        # print '****** a star initialisation ******'
-        # print 'Map in A star Classs    ', the_map
-        # print
+    def __init__(self, the_map ):
 
-        self.n = 10 #len (the_map[0])
+        self.n = 10
         self.m = len(the_map)
         self.the_map = the_map
         self.directions = 4
-        self.dx = dx
-        self.dy = dy
+        self.dx = [1, 0, -1, 0]
+        self.dy = [0, 1, 0, -1]
 
     def pathFind(self, xStart, yStart, xFinish, yFinish):
         closed_nodes_map = []
@@ -102,8 +99,8 @@ class a_star:
 
 
             for i in range(self.directions):
-                xdx = x + self.dx
-                ydy = y + self.dy
+                xdx = x + self.dx[i]
+                ydy = y + self.dy[i]
                 if not (xdx < 0 or xdx > self.n-1 or ydy < 0 or ydy > self.m - 1
                         or self.the_map[ydy][xdx] == 1 or closed_nodes_map[ydy][xdx] == 1):
 
