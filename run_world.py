9770294cd2f86bcd08aa2bb6b267409bb1204478
ElnazShy <elnaz.shafipour@gmail.com>
2018-03-29 14:42:54 +0100

    Remove the dependency of code to the_map
    and make the code cleaner.

--- run_world.py
+++ run_world.py
@@ -2,7 +2,7 @@ import agent
 import item
 import random
 import simulator
-import UCT
+
 
 
 radius_max = 1
@@ -46,12 +46,12 @@ def initialize_items_agents_notrandom(n, m):
 
     # creating agent
     (x, y) = (4,4)
-    unknown_agent = agent.Agent(x, y, 0,'l1', 0)
+    unknown_agent = agent.Agent(x, y, 'l1', 0)
     the_map[y][x] = 8
     agents.append(unknown_agent)
 
     (x, y) = (1, 1)
-    main_agent = agent.Agent(x, y,0,'l1', 1)
+    main_agent = agent.Agent(x, y,'l1', 1)
     main_agent.level = 1
     main_agent.set_level(1)
     #agents.append(main_agent)
@@ -86,14 +86,7 @@ def initialize_items_agents(n, m):
     sf.remove((x, y))
     the_map[x][y] = 9
 
-    (x, y) = random.choice(sf)
-    main_agent = agent.Agent(x, y, 'l1', -1)
-    main_agent.set_level(1)
-    main_agent.direction = 0
 
-    sf.remove((x, y))
-
-    return main_agent
 
 
 def create_empty_map(n,m):
@@ -125,12 +118,12 @@ def create_tmp_map(items, agents, main_agent):
 
     (a_agent_x, a_agent_y) = agents[0].get_position()
     local_map[a_agent_y][a_agent_x] = 8
-    local_agent = agent.Agent(a_agent_x, a_agent_y,agents[0].direction, 'l1', 0)
+    local_agent = agent.Agent(a_agent_x, a_agent_y, 'l1', 0)
     local_agents.append(local_agent)
 
     (m_agent_x, m_agent_y) = main_agent.get_position()
     local_map[m_agent_y][m_agent_x] = 9
-    local_main_agent = agent.Agent(m_agent_x, m_agent_y,main_agent.direction, 'l1', -1)
+    local_main_agent = agent.Agent(m_agent_x, m_agent_y, 'l1', -1)
     local_main_agent.set_level(main_agent.level)
 
     return local_agents,local_items,local_map,local_main_agent
@@ -168,9 +161,8 @@ unknown_agent.set_parameters(true_level, true_radius, true_angle)
 # ======================================================================================================
 
 real_sim.draw_map()
-# real_sim.draw_map_with_level()
-main_agent.direction = 0
-print ("Main agent's direction:",main_agent.get_agent_direction( ) )
+
+
 time_step = 0
 while time_step < 100:
 
@@ -181,26 +173,17 @@ while time_step < 100:
     prev_sim = simulator.simulator(local_map, local_items, local_agents, local_main_agent, 10, 10)
 
     unknown_agent = real_sim.run_and_update(unknown_agent)
-    main_agent_next_action = UCT.move_agent(real_sim.agents, real_sim.items,  real_sim.main_agent, true_parameters)
-    print("main_agent_next_action: ", main_agent_next_action)
-
-    ## TODO: Load action is not correct
-    print ("Main agent's direction:",main_agent.get_agent_direction( ) )
-
-    r = UCT.do_move(real_sim, main_agent_next_action)
 
     time_step = time_step + 1
 
     real_sim.draw_map()
-    # real_sim.draw_map_with_level()
 
     if real_sim.items_left() == 0:
         break
     print "left items", real_sim.items_left()
 
 
+
 print  time_step
-print "True parameters: ",true_level,true_radius,true_angle
-#print "last new_estimated_parameters", new_estimated_parameters
 
 
